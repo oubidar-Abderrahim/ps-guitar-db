3b9f46ccc2a806524bcf7c534b5d71f9ca1cd73a
aoubidar <aoubidar@MAPF1GTABW.CSCMWS.CSCMWS.COM>
2019-04-15 17:03:24 +0100

    Jpa repo

--- src/test/java/com/guitar/db/LocationPersistenceTests.java
+++ src/test/java/com/guitar/db/LocationPersistenceTests.java
@@ -1,6 +1,7 @@
 package com.guitar.db;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 import java.util.List;
 
@@ -15,6 +16,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.guitar.db.model.Location;
+import com.guitar.db.repository.LocationJpaRepository;
 import com.guitar.db.repository.LocationRepository;
 
 @ContextConfiguration(locations={"classpath:com/guitar/db/applicationTests-context.xml"})
@@ -23,8 +25,22 @@ public class LocationPersistenceTests {
 	@Autowired
 	private LocationRepository locationRepository;
 
+	@Autowired
+	private LocationJpaRepository locationJpaRepository;
+	
+	
+	
 	@PersistenceContext
 	private EntityManager entityManager;
+	
+	@Test
+	public void testJpaFind() {
+		List<Location> locationsByJpa = locationJpaRepository.findAll();
+		assertNotNull(locationsByJpa);
+		
+		
+		locationJpaRepository.
+	}
 
 	@Test
 	@Transactional
